682c72b2fb5e6b2473cf2c79373dc6eac9c30d1c
Alan McGinlay <amcginlay@users.noreply.github.com>
2022-11-06 11:56:15 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 $pick = array("SERVER_PROTOCOL", "REQUEST_URI", "SCRIPT_NAME", "HTTP_HOST");
-foreach ($_SERVER as $key => $value) {
-    if (in_array($key, $pick)) {
-        echo $key.': '.$value.'<br>';
-    }
-}
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Test Container</title>
+  </head>
+  <body>
+    <?php foreach ($_SERVER as $key => $value) { if (in_array($key, $pick)) { ?>
+        <div>
+          <p><?php echo $key.': '.$value; ?></p>
+        </div>
+    <?php } } ?>
+  </body>
+</html>
